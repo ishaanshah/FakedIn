20fd457a6ab0a6e5f8da3cd807114786ed8fbbf2
Ishaan Shah <ishaan.n.shah@gmail.com>
2021-01-21 12:57:23 +0530

    Add loader to apply modal

--- frontend/src/components/applicant/ApplyModal.tsx
+++ frontend/src/components/applicant/ApplyModal.tsx
@@ -5,6 +5,8 @@ import Modal from "react-bootstrap/Modal";
 import * as Yup from "yup";
 import axios from "axios";
 import { store } from "react-notifications-component";
+import Spinner from "react-bootstrap/Spinner";
+import { useState } from "react";
 
 type ApplyModalProps = {
   jobId: string;
@@ -17,12 +19,15 @@ const ApplicationSchema = Yup.object().shape({
 });
 
 function ApplyModal({ jobId, showModal, setShowModal }: ApplyModalProps) {
+  const [loading, setLoading] = useState(false);
+
   const formik = useFormik({
     initialValues: {
       sop: "",
     },
     validationSchema: ApplicationSchema,
     onSubmit: async (values) => {
+      setLoading(true);
       try {
         const response = await axios.post(`/api/jobs/apply/${jobId}`, values, {
           headers: {
@@ -54,40 +59,56 @@ function ApplyModal({ jobId, showModal, setShowModal }: ApplyModalProps) {
             showIcon: true,
           },
         });
+      } finally {
+        setLoading(false);
       }
     },
   });
 
   return (
-    <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+    <Modal
+      show={showModal}
+      onHide={() => setShowModal(false)}
+      contentClassName="justify-content-center"
+      centered
+    >
+      {loading && (
+        <Spinner
+          animation="border"
+          className="align-self-center"
+          style={{ position: "absolute", zIndex: 100 }}
+        />
+      )}
       <Form onSubmit={formik.handleSubmit}>
         <Modal.Header closeButton>
           <Modal.Title>Statement of Purpose</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Form.Group controlId="sop">
-            <Form.Control
-              value={formik.values.sop}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              isInvalid={formik.touched.sop && !!formik.errors.sop}
-              placeholder="Enter your statement of purpose..."
-              as="textarea"
-              rows={4}
-            />
-            <Form.Control.Feedback type="invalid">
-              {formik.errors.sop}
-            </Form.Control.Feedback>
-          </Form.Group>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="outline-dark" onClick={() => setShowModal(false)}>
-            Close
-          </Button>
-          <Button variant="outline-success" type="submit">
-            Submit
-          </Button>
-        </Modal.Footer>
+        <div style={{ opacity: loading ? 0.5 : 1 }}>
+          <Modal.Body>
+            <Form.Group controlId="sop">
+              <Form.Control
+                value={formik.values.sop}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                isInvalid={formik.touched.sop && !!formik.errors.sop}
+                placeholder="Enter your statement of purpose..."
+                as="textarea"
+                rows={4}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.sop}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="outline-dark" onClick={() => setShowModal(false)}>
+              Close
+            </Button>
+            <Button variant="outline-success" type="submit">
+              Submit
+            </Button>
+          </Modal.Footer>
+        </div>
       </Form>
     </Modal>
   );
